2b651ff9a9ae78aab02f17ee710144385b0cd376
Lardomir <126703864+Lardomir@users.noreply.github.com>
2026-09-02 10:11:45 +0200

    Fix database repository loading (#3)

--- repositoryFactory.js
+++ repositoryFactory.js
@@ -1,12 +1,12 @@
 const path = require("path");
-const PostRepository = require("./postRepository");
-const PostgresRepository = require("./postgresRepository");
 
 function createRepository() {
   if (process.env.DATABASE_URL) {
+    const PostgresRepository = require("./postgresRepository");
     return new PostgresRepository(process.env.DATABASE_URL);
   }
 
+  const PostRepository = require("./postRepository");
   return new PostRepository(
     process.env.DB_PATH || path.join(__dirname, "data", "postiliste.db")
   );
